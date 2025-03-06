46d754c3d1edc994cc5789dab342db743f37593b
Flitzhans <noemanateeq02@gmail.com>
2025-03-06 21:30:03 +0300

    Added HTML & JS files

---
+++ Redirect/Syntax.js
@@ -0,0 +1,19 @@
+//console.log("YO FAM")
+// count = 0
+
+// div = document.getElementById("div")
+// button = document.getElementById("button")
+// function clickCounter(){
+//     count += 1
+//     div.innerHTML = "You clicked the button " + count + " times"
+// }
+
+// button.addEventListener("click", clickCounter)
+
+
+button = document.getElementById("button")
+function Redirect(){
+    window.location = "Buttons2.html"
+}
+
+button.addEventListener("click", Redirect)
